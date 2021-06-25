21457e6b4149435355b83990c5be82da0de86d46
Sebastien Chassande <chassande@gmail.com>
2021-06-25 12:40:06 +0200

    ajust cc

--- functions/src/send-validation-required.ts
+++ functions/src/send-validation-required.ts
@@ -8,39 +8,44 @@ export function func(request:any, response:any, ctx:any):Promise<any> {
     return common.loadUser(ctx.db, request.body.data.userId, response)
     .then( (user: User) => {
         //Build email
-        const subject = '[Upgrade.CoachReferee.com] validation required';
-        let ccEmails: string[] = [ctx.gmailEmail, user.email];
-        if (request.body.data.ccEmails && request.body.data.ccEmails.length > 0) {
-            ccEmails = ccEmails.concat(request.body.data.ccEmails);
-        }
+        const subject = '[Upgrade.CoachReferee.com] Validation required: ' + user.shortName;
         let rolesToValidate = '';
         if (request.body.data.rolesToValidate && request.body.data.rolesToValidate.length > 0) {
             rolesToValidate = request.body.data.rolesToValidate.join(', ');
         }
-        let toEmails: string = request.body.data.toEmails;
+        let toEmails: string[] = request.body.data.toEmails;
         let hi: string;
         if (toEmails.length === 0) {
-            hi = '';
+            hi = 'Hi';
         } else if (rolesToValidate === 'REFEREE') {
-            hi = 'the NDR of ' + user.country;
+            hi = 'Hi the NDR of ' + user.country;
         } else {
-            hi = 'Admin';
+            hi = 'Hi Admin';
         }
-        // ensure to have at one toEmail destination
+        const minCCEmails = [ctx.gmailEmail, user.email]; // emails always in CC
+        let ccEmails: string[] = [];
+        const hasCC = request.body.data.ccEmails && request.body.data.ccEmails.length > 0;
+        if (hasCC) {
+            ccEmails = ccEmails.concat(request.body.data.ccEmails);
+        }
+        // ensure to have at least one toEmail destination
         if (toEmails.length === 0) {
-            if (request.body.data.ccEmails && request.body.data.ccEmails.length > 0) {
-                toEmails = request.body.data.ccEmails;            
+            if (hasCC) {
+                // put the cc email as to
+                toEmails = ccEmails;
+                ccEmails = [];
             }
             if (toEmails.length === 0) {
+                // put the application manager as to
                 toEmails = ctx.gmailEmail;
             }
         }
         const email = {
             from: ctx.gmailEmail,
-            cc: ccEmails,
+            cc: ccEmails.concat(minCCEmails),
             to: toEmails,
             subject,
-            html: `Hi ${hi}, 
+            html: `${hi}, 
                 <p>${user.firstName} ${user.lastName} asks new role(s) in the Upgrade web application: ${rolesToValidate}
                 <br>Please go to the <a href="https://upgrade.coachreferee.com">Upgrade application</a> in order to validate the asked role(s).</p>
                 <p>Best regard
