6fb7412280b1984d7eef08f553dabf55caaf6b0e
Sebastien Chassande <chassande@gmail.com>
2022-07-27 07:09:31 +0200

    version

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -8,5 +8,5 @@ export const environment = {
     storageBucket: 'refcoach-676e3.appspot.com',
     messagingSenderId: '252857255317'
   },
-  version: '2.13.0-2022-07-26T05:44:22.785Z'
+  version: '2.14.0-2022-07-27T05:04:40.880Z'
 };
